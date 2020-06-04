68500d1bd44abbd8c885ad15609f80cdca62505f
mtorres9095 <mtorres9095@gmail.com>
2020-06-04 17:59:42 -0400

    Add project files.

---
+++ Tech Jobs Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Jobs_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
